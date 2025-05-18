bba44a3750f7d8080e1eddf1a13cdae95e30787b
mehrcode <mehrnoosh.shoolestani@gmail.com>
2025-05-18 05:55:33 +0330

    update header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className="bg-[#fef8e0] text-black font-sans">
-        <header className="w-full p-4 bg-[#4b2e83] text-white shadow-md flex flex-col gap-x-6 justify-between">
+        <header className="w-full p-4 bg-[#4b2e83] text-white shadow-md flex flex-row gap-x-6 justify-between">
           <Link href="/" className="text-lg font-bold hover:underline">Home</Link>
           <span className="text-sm opacity-80">Made With 💜 by MehrCode</span>
         </header>
